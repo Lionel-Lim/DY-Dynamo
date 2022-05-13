0dc5e6e6f8353b69ebc70b99222811dd75be40a6
DY Lim <dlagus6412@gmail.com>
2022-05-13 19:17:24 +0800

    Set UI of Alignment Manager

---
+++ script/Python/Align.extension/Align.tab/Create.panel/CreateAlignment.pushbutton/script.py
@@ -0,0 +1,85 @@
+from System.Collections.ObjectModel import ObservableCollection
+
+import rpw
+from rpw import revit, db, ui, DB, UI
+
+from rpw.exceptions import RevitExceptions
+from pyrevit.forms import WPFWindow
+
+from event import CustomizableEvent
+
+__doc__ = "Alignment Manager"
+__title__ = "Create Alignment Model"
+__author__ = "DY Lim"
+__persistentengine__ = True
+
+
+def debug(self, line):
+    self.debug.Text = "{}\n{}".format(self.debug.Text, line)
+    self.debug.ScrollToEnd()
+
+
+class verticalAlignmentFormat:
+    def __init__(
+        self,
+        index=None,
+        pviStation=None,
+        pviElevation=None,
+        grade=None,
+        _CurveType=None,
+        _CurveLength=None,
+        _K=None,
+    ):
+        self.index = index
+        self.pviStation = pviStation
+        self.pviElevation = pviElevation
+        self.grade = grade
+        self._CurveType = _CurveType
+        self._CurveLength = _CurveLength
+        self._K = _K
+
+
+curveType = ["Line", "Curve"]
+
+
+class form_window(WPFWindow):
+    def __init__(self, xaml_file_name):
+        WPFWindow.__init__(self, xaml_file_name)
+        self.Show()
+
+        self.VAContents = ObservableCollection[object]()
+        self.VAContents.Add(verticalAlignmentFormat(0))
+        self.VerticalCurveTable.ItemsSource = self.VAContents
+        self.curveType = ObservableCollection[object]()
+        [self.curveType.Add(ct) for ct in curveType]
+        self.curveTypeBox.ItemsSource = self.curveType
+        self.debug.Text = "Initializing Success\nWaiting Task..."
+
+    def addrow(self, sender, e):
+        for i in self.VAContents:
+            lastIndex = i.index
+        self.VAContents.Add(verticalAlignmentFormat(lastIndex + 1))
+        #
+        debug(self, "{} Row Added".format(lastIndex + 1))
+
+    def clearRow(self, sender, e):
+        selectedIndex = self.VerticalCurveTable.SelectedIndex
+        if selectedIndex == -1:
+            debug(self, "Please Select Row To Be Cleared".format(selectedIndex))
+        else:
+            self.VAContents.RemoveAt(selectedIndex)
+            self.VAContents.Insert(
+                selectedIndex, verticalAlignmentFormat(selectedIndex)
+            )
+            debug(self, "{} Row Cleared".format(selectedIndex))
+
+    def removeRow(self, sender, e):
+        index = self.VAContents.Count - 1
+        if index == 0:
+            debug(self, "Cannot Remove All")
+        else:
+            self.VAContents.RemoveAt(index)
+            debug(self, "{} Row Removed".format(index))
+
+
+form = form_window("ui.xaml")
