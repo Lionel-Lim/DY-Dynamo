886dbd8808f4a8f0b6f674a0d3cf54cb85793561
Lionel-Lim <dlagus6412@gmail.com>
2022-06-13 22:29:57 +0800

    Update test.py

--- script/Python/Align.extension/lib/test.py
+++ script/Python/Align.extension/lib/test.py
@@ -1,8 +1,27 @@
-import CurveProcessor
+# Load the Python Standard and DesignScript Libraries
+import sys
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+sys.path.append('C:\Users\DY\Documents\GitHub\DY-Dynamo\script\Python\Align.extension\lib')
 
-pt1 = CurveProcessor.VectorEntity(10,50)
-pt2 = CurveProcessor.VectorEntity(100,20)
-create = CurveProcessor.CreateEntity()
-vec = create.Vector_ByTwoPoints(pt1,pt2).normalise()
-vec_en = CurveProcessor.VectorEntity(1,2)
-print(vec())
+import CurveProcessor as CP
+
+# The inputs to this node will be stored as a list in the IN variables.
+dataEnteringNode = IN
+isArc = IN[0]
+vert3 = IN[1]
+vert2 = IN[2]
+create = CP.CreateEntity()
+data= []
+for i in range(len(isArc)):
+	if isArc[i]:
+		pts = vert3[i]
+		data.append(create.ArcByThreePoints(pts[0],pts[2],pts[1]))
+	else:
+		pts = vert2[i]
+		data.append(CP.LineEntity(pts[0],pts[1]))
+
+
+
+OUT = data
